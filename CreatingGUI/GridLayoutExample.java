70f7ca6fdb527aa5860a951e6e4e80b9e79f48a5
milanneupanee <milanneupane74@gmail.com>
2023-06-28 17:53:33 +0545

    first commit

---
+++ CreatingGUI/GridLayoutExample.java
@@ -0,0 +1,20 @@
+package CreatingGUI;
+import javax.swing.*;
+import java.awt.*;
+public class GridLayoutExample extends JFrame {
+    JButton []btn=new JButton[11];
+    GridLayoutExample(){
+        for(int i=0;i<btn.length;i++){
+            btn[i]=new JButton("B"+(i+1));
+            add(btn[i]);
+        }
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setSize(300,300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new GridLayoutExample();
+    }
+}
